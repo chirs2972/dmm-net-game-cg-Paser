cfee9a03b4cb9bbce7b69f3e76ea4594654b6a63
chirs2972 <chirs2972@gmail.com>
2020-05-26 00:28:28 +0800

    initial
    initial

---
+++ delete same picture in the same folder.py
@@ -0,0 +1,31 @@
+#要放同個資料夾
+import os
+import hashlib
+def filecount():
+  filecount = int(os.popen('dir /B |find /V /C ""').read())
+  return (filecount)
+def md5sum(filename):
+  f = open(filename, 'rb')
+  md5 = hashlib.md5()
+  while True:
+    fb = f.read(8096)
+    if not fb:
+      break
+    md5.update(fb)
+  f.close()
+  return (md5.hexdigest())
+def delfile():
+  all_md5 = {}
+  filedir = os.walk(os.getcwd())
+  for i in filedir:
+    for tlie in i[2]:
+      if md5sum(tlie) in all_md5.values():
+        os.remove(tlie)
+      else:
+        all_md5[tlie] = md5sum(tlie)
+if __name__ == '__main__':
+  oldf = filecount()
+  print('去重前有', oldf, '个文件\n\n\n请稍等正在删除重复文件...')
+  delfile()
+  print('\n\n去重后剩', filecount(), '个文件')
+  print('\n\n一共删除了', oldf - filecount(), '个文件\n\n')
